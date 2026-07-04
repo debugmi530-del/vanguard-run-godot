ad600f03ebf2f1decd8b88e47beddcaf8b41088f
debugmi530-del <debugmi530@gmail.com>
2026-07-04 09:49:07 +0300

    Add scripts/Phys.gd

---
+++ scripts/Phys.gd
@@ -0,0 +1,10 @@
+extends RefCounted
+class_name Phys
+
+## Shared platforming physics constants.
+
+const GRAVITY := 2200.0
+const JUMP_VELOCITY := -820.0
+const MOVE_SPEED := 320.0
+const MAX_FALL_SPEED := 1400.0
+const PLAYER_SIZE := Vector2(36, 52)
